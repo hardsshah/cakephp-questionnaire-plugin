65d1b88dab07ca876cacf337b21e815df99f595f
Jose Gonzalez <myaccounts@savant.be>
2009-07-16 11:09:29 -0400

    missing ;

--- questionnaire_app_model.php
+++ questionnaire_app_model.php
@@ -2,6 +2,6 @@
 class QuestionnaireAppModel extends AppModel {
 	var $recursive = -1;
 	var $actsAs = array('Containable');
-	var $useDbConfig = 'questionnaire'
+	var $useDbConfig = 'questionnaire';
 }
 ?>
